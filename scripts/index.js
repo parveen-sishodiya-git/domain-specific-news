f91ccc725ce242da18140ebc99349bf1c5c3cf36
parveen-sishodiya-bitbucket <parveen.sishodiya.official@gmail.com>
2020-10-13 12:49:19 +0530

    Added loader

--- scripts/index.js
+++ scripts/index.js
@@ -12,41 +12,21 @@ const apiKey = "mP9TmBctdqcRjsnFviBw9FrUD6r80dnE"; //NYT wire feeds key
 const apiURL = `https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=${apiKey}`;
 
 
-
+setLoader();
 
 let xhr = new XMLHttpRequest();
 xhr.open("GET", apiURL, true);
 
 xhr.onprogress = function () {
-  let loader = `<div class="spinner-grow text-primary" role="status">
-    <span class="sr-only">Loading...</span>
-  </div>
-  <div class="spinner-grow text-secondary" role="status">
-    <span class="sr-only">Loading...</span>
-  </div>
-  <div class="spinner-grow text-success" role="status">
-    <span class="sr-only">Loading...</span>
-  </div>
-  <div class="spinner-grow text-danger" role="status">
-    <span class="sr-only">Loading...</span>
-  </div>
-  <div class="spinner-grow text-warning" role="status">
-    <span class="sr-only">Loading...</span>
-  </div>
-  <div class="spinner-grow text-info" role="status">
-    <span class="sr-only">Loading...</span>
-  </div>
-  <div class="spinner-grow text-dark" role="status">
-    <span class="sr-only">Loading...</span>
-  </div>`;
-  console.log("Processing");
-  document.getElementById("newsCard").innerHTML = loader;
-
+  console.log("onprogress started");
+ 
+  console.log("onprogress ended");
 };
 
 xhr.onload = function () {
+  console.log("Onload strted");
   // console.log(this.responseText);
-  console.log("*****************************");
+  // console.log("*****************************");
   let news = JSON.parse(this.responseText)
   let articles = news.results;
   let imageURL = "";
@@ -58,44 +38,63 @@ xhr.onload = function () {
     for (urlkey in articles[article].multimedia) {
       // console.log(urlkey);
       imageURL = articles[article].multimedia[2].url;
-      console.log("LOOP uRLLLLLLLLLL", articles[article].multimedia[2].url);
-
+      // console.log("LOOP uRLLLLLLLLLL", articles[article].multimedia[2].url);
+      
     }
-    if(imageURL === ""){
-      imageURL ="assets/pictures/defaultNewsImage.PNG";
+    if (imageURL === "") {
+      imageURL = "assets/pictures/defaultNewsImage.PNG";
     }
     // console.log("uRLLLLLLLLLL",articles[article].multimedia[2].url);
     html += `
-        <div style="width: 384px;height: 767px;padding: 49px 0px 20px 25px;" class="backImage ml-3 mb-3">
-        <div style="width: 329px;height: 685px;" >
-            <div class="card" style="max-width: 21rem;">
-                <img src="${imageURL}" class="card-img-top" alt="...">
-<div class="card-body">
+    <div style="width: 384px;height: 767px;padding: 49px 0px 20px 25px;" class="backImage ml-3 mb-3">
+    <div style="width: 329px;height: 685px;" >
+    <div class="card" style="max-width: 21rem;">
+    <img src="${imageURL}" class="card-img-top" alt="...">
+    <div class="card-body">
     <h5 class="card-title">${articles[article].title}</h5>
     <p class="card-text">${articles[article].abstract}</p>
-</div>
-<ul class="list-group list-group-flush">
+    </div>
+    <ul class="list-group list-group-flush">
     <li class="list-group-item">Author : ${articles[article].source}</li>
     <li class="list-group-item">${articles[article].updated_date}</li>
-</ul>
-<div class="card-body">
+    </ul>
+    <div class="card-body">
     <a href="${articles[article].url}" target="_blank" class="card-link">Read more</a>
-</div>
-</div>
-</div>
-</div>
-     `;
+    </div>
+    </div>
+    </div>
+    </div>
+    `;
   }
-
+  console.log("setting news");
+  
   document.getElementById("newsCard").innerHTML = html;
-
-  console.log("done");
+  
+  console.log("Onload ended");
 };
 
 xhr.send();
 
 
+function setLoader() {
+  console.log("setting loader");
+  let loader = `<div style="padding: 8em 35em">
+  <div class="spinner-grow text-primary" role="status">
+  <span class="sr-only">Loading...</span>
+  </div>
+  <div class="spinner-grow text-secondary" role="status">
+  <span class="sr-only">Loading...</span>
+  </div>
+  <div class="spinner-grow text-success" role="status">
+  <span class="sr-only">Loading...</span>
+  </div>
+  <div class="spinner-grow text-danger" role="status">
+  <span class="sr-only">Loading...</span>
+  </div>
+</div>`;
 
+  document.getElementById("newsCard").innerHTML = loader;
+}
 
 
 
